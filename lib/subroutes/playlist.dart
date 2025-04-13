a20f5ea1afcf6a53876e0ca132798017ac765c76
barrett <onkoe@protonmail.com>
2025-04-13 14:19:56 -0500

    feat(subroutes/playlist): add basic playlist view

---
+++ lib/subroutes/playlist.dart
@@ -0,0 +1,49 @@
+//! A playlist you can browse through.
+
+import 'package:flutter/material.dart';
+
+import '../components/songlist.dart';
+import '../tabs/library.dart';
+
+class PlaylistPage extends StatefulWidget {
+  final LibraryEntry entry;
+  static final String route = "recommendations";
+
+  const PlaylistPage({super.key, required this.entry});
+
+  @override
+  State<PlaylistPage> createState() => _PlaylistPageState();
+}
+
+class _PlaylistPageState extends State<PlaylistPage> {
+  late final LibraryEntry entry;
+
+  @override
+  void initState() {
+    super.initState();
+    entry = widget.entry;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(entry.name),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // show playlist art and name
+            Text("playlist art and name"),
+
+            // show all the songs in the playlist
+            Expanded(
+              child: SongList(songs: entry.songs),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
