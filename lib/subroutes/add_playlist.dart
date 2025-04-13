fa9a1abd60a55dcd2c2d9537ba862e35c8f90cf3
barrett <onkoe@protonmail.com>
2025-04-13 14:21:23 -0500

    feat(subroutes/add_playlist): basic layout

--- lib/subroutes/add_playlist.dart
+++ lib/subroutes/add_playlist.dart
@@ -2,8 +2,26 @@
 
 import 'package:flutter/material.dart';
 
-class AddPlaylistPage extends StatelessWidget {
+import '../components/songlist.dart';
+import '../tabs/library.dart';
+
+class AddPlaylistPage extends StatefulWidget {
   const AddPlaylistPage({super.key});
+
+  final String title = "Add new playlist";
+  static final String route = "add_playlist";
+
+  @override
+  State<AddPlaylistPage> createState() => _AddPlaylistPageState();
+}
+
+class _AddPlaylistPageState extends State<AddPlaylistPage> {
+  List<Song> songs = List.empty(growable: true);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +31,25 @@ class AddPlaylistPage extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(24),
-        child: Text('TODO'),
+        child: Column(children: [
+          // empty art
+          Image.network("https://picsum.photos/256",
+              width: 128, height: 128, fit: BoxFit.cover),
+          Text("Art", style: Theme.of(context).textTheme.titleLarge),
+          Text("Adjust your playlist's art",
+              style: Theme.of(context).textTheme.bodyLarge),
+
+          // naming the playlist
+          Text("Name"),
+          Text("Give your playlist a name"),
+          TextField(),
+
+          // adding songs
+          Text("Songs"),
+          Text(
+              "Add, remove, and sort songs in the list below. You can always change these later."),
+          SongList(songs: songs),
+        ]),
       ),
     );
   }
