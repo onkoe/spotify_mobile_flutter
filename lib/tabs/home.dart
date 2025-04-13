d92ee94f4490cb185fe2466a8f69189f84afbf3b
barrett <onkoe@protonmail.com>
2025-04-12 23:05:21 -0500

    feat(tabs/home): make hoz list function

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -52,3 +52,43 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+/// creates a horizontal list for the home page.
+Container makeHozList(
+    BuildContext context, List<Widget> listElements, String title) {
+  return Container(
+    margin: const EdgeInsets.all(8.0),
+    padding: const EdgeInsets.all(16.0),
+    // padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
+    decoration: BoxDecoration(
+      color: Theme.of(context).colorScheme.surfaceContainerHigh,
+      borderRadius: BorderRadius.circular(16.0),
+    ),
+    child: Column(
+        // padding between each element
+        spacing: 4.0,
+
+        // children widgets
+        children: [
+          // title
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              title,
+              style: TextTheme.of(context).titleLarge,
+              textWidthBasis: TextWidthBasis.parent,
+              textAlign: TextAlign.left,
+            ),
+          ),
+
+          // hoz. list of the media collections
+          SizedBox(
+            height: 128,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: listElements,
+            ),
+          ),
+        ]),
+  );
+}
