99e7101fe6d757cd13061e715b022e04af44dfff
barrett <onkoe@protonmail.com>
2025-04-14 22:04:53 -0500

    feat(sub/now_playing): add all elements

--- lib/subroutes/now_playing.dart
+++ lib/subroutes/now_playing.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/now_playing_model.dart';
+import '../types.dart';
 
 class NowPlayingPage extends StatelessWidget {
   const NowPlayingPage({super.key});
@@ -15,29 +20,88 @@ class NowPlayingPage extends StatelessWidget {
 
       // add playback stuff here
       body: Center(
-        child: Column(
-          children: [
+        child:
             // giant album art
+            Consumer<NowPlayingModel>(
+          builder: (context, info, child) {
+            Song? song = info.nowPlaying;
 
-            // title
-
-            // artist
-
-            // controls
-            Row(
+            return Column(
               children: [
-                // shuffle
+                // giant album art
+                () {
+                  if (song != null && song.art != null) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: CachedNetworkImage(
+                        imageUrl: song.art!,
+                        width: 256,
+                        height: 256,
+                      ),
+                    );
+                  } else {
+                    return Placeholder(fallbackWidth: 48, fallbackHeight: 48);
+                  }
+                }(),
 
-                // skip back
+                // title
+                Text(song?.title ?? "Not playing"),
 
-                // play
+                // artist
+                Text(song?.artist ?? "Unknown artist"),
 
-                // skip next
+                // controls
+                Row(
+                  children: [
+                    // shuffle
+                    IconButton(
+                      icon: () {
+                        if (info.shuffle == Shuffle.on) {
+                          return Icon(Icons.shuffle_on);
+                        } else {
+                          return Icon(Icons.shuffle);
+                        }
+                      }(),
+                      onPressed: () => info.toggleShuffle(),
+                    ),
 
-                // repeat
+                    // skip back
+                    IconButton(
+                      icon: Icon(Icons.skip_previous),
+                      onPressed: () => info.skipBack(),
+                    ),
+
+                    // play
+                    IconButton.filled(
+                      icon: Icon(Icons.play_arrow),
+                      onPressed: () => info.skipBack(),
+                    ),
+
+                    // skip next
+                    IconButton(
+                      icon: Icon(Icons.skip_next),
+                      onPressed: () => info.skipNext(),
+                    ),
+
+                    // repeat
+                    IconButton(
+                      icon: () {
+                        switch (info.repeat) {
+                          case RepeatSetting.queue:
+                            return Icon(Icons.repeat_on);
+                          case RepeatSetting.single:
+                            return Icon(Icons.repeat_one);
+                          case RepeatSetting.off:
+                            return Icon(Icons.repeat);
+                        }
+                      }(),
+                      onPressed: () => info.nextRepeatMode(),
+                    ),
+                  ],
+                ),
               ],
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
